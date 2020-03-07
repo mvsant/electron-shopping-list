af1164d809b087c30357e81cc4cad0db57881570
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-07 14:22:34 -0300

    Create delete items funcionality.

--- src/index.js
+++ src/index.js
@@ -75,7 +75,10 @@ const mainMenuTemplate = [
         }
       },
       {
-        label: 'Clear items'
+        label: 'Clear items',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
       },
       {
         accelerator: 'CmdOrCtrl+Q',
